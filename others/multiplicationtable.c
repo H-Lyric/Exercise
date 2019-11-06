ae7d3cc33e478ebc30351e0f66965cdedd545ce8
A.Mex <180300365@ucaribe.edu.mx>
2019-11-05 21:46:41 -0500

    Added a new exercise

---
+++ others/multiplicationtable.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main ()
+{
+ int N;
+ int Total;
+ int i;
+
+ 	scanf ("%i", &N );
+	for (i = 1; i <= N; i++)
+		{
+		Total = N * i;
+		}
+ 	printf ("%i", Total);
+	
+return 0 ;
+
+}
+
